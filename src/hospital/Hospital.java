4eb42cb99544c7a5f0d8e07b3d5ba01ce82c0661
Lucas Penteado <145309520+pentiadu@users.noreply.github.com>
2023-12-07 11:51:28 -0300

    Add files via upload

---
+++ src/hospital/Hospital.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package hospital;
+
+/**
+ *
+ * @author lucas
+ */
+public class Hospital {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+                java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Login().setVisible(true);
+            }
+        });
+        
+    }
+    
+}
